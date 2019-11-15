4c15226dc2a63e5770a9c60ba03aef8773f11627
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-11-15 21:31:06 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,6 +15,11 @@ def create_project_hash
 
 projects = {}
 
+kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+    
+    projects
+  end
 end
 
 create_project_hash
